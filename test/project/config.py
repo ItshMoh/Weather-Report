0f39b36ebf8aea56d9ecc204bb162e5bd3a258ab
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-10-24 22:08:23 +0530

    Updated the config.py file

--- test/project/config.py
+++ test/project/config.py
@@ -1,2 +1,2 @@
-WEATHER_API_KEY = '6218725b6d844f7ab0b64038242809'  # Get this from OpenWeatherMap or any weather API
-WEATHER_API_URL = 'http://api.weatherapi.com/v1/current.json?key=6218725b6d844f7ab0b64038242809&q=${location}&aqi=yes`'
+WEATHER_API_KEY = ''  # You Can Get this from  weatherapi.com
+WEATHER_API_URL = 'http://api.weatherapi.com/v1/current.json?key=WEATHER_API_KEY&q=${location}&aqi=yes`'
